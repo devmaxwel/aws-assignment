9b624ebe6a04626b66971b4fdee27ce58d8315b7
devmaxwel <eng.maxwel.ochieng@gmail.com>
2023-04-14 22:55:35 +0300

    see the app on aws

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav className="nav">
+          <div className="log0">
+            {/* Logo */}
+            Ak_Rosh_Sree
+          </div>
+          <div className="items">
+            {/* Links */}
+            <ul className="">
+              <li>Luggage</li>
+              <li>Bag Packs</li>
+              <li>Hand Bags</li>
+              <li>Hand Bags</li>
+              <li>Wallets</li>
+            </ul>
+          </div>
+        </nav>
       </header>
+      <div></div>
     </div>
   );
 }
